b8c1f3a0512e683250a8b9629d5d1aa03c0667d4
Sindile <smakhathini24@gmail.com>
2022-02-16 16:33:03 +0200

    frontend

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import VeeValidate from 'vee-validate';
+import { FontAwesomeIcon } from './plugins/font-awesome'
+createApp(App)
+  .use(router)
+  .use(store)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
